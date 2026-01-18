4d9ee6e218d02e451d40b6ae28e07a075e01f2de
Ifal Fahri A <ifalfahri16@gmail.com>
2026-01-18 11:59:48 +0800

    refactor: Adjust footer layout, text size, and spacing for improved mobile responsiveness.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,13 +6,13 @@ export default function Footer() {
   return (
     <footer className="bg-neutral-950 py-24 sticky bottom-0 px-4 md:px-8 border-t border-white/5">
       <Reveal>
-        <div className="mx-auto max-w-7xl flex flex-col md:flex-row justify-between items-end">
-          <div>
-            <h2 className="text-[15vw] font-black leading-none text-neutral-900 select-none tracking-tighter">
+        <div className="mx-auto max-w-7xl flex flex-col-reverse md:flex-row justify-between items-end">
+          <div className="w-full text-center md:w-auto md:text-left">
+            <h2 className="text-[26vw] md:text-[15vw] font-black leading-none text-neutral-900 select-none tracking-tighter">
               GOLDA
             </h2>
           </div>
-          <div className="flex flex-col md:flex-row gap-12 text-neutral-400 mt-8 md:mt-0 font-medium">
+          <div className="flex flex-row gap-12 text-neutral-400 mt-8 md:mt-0 mb-8 font-medium">
             <a href="#" className="hover:text-amber-500 transition-colors">
               INSTAGRAM
             </a>
